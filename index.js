0c1c3558c84f6653271a0869787b9f0861d61596
elguesabal <joseelguesabal@gmail.com>
2025-09-08 00:41:05 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Te amo");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
