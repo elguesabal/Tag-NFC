123cb2569b6b05c6e27077b7caa0ae7b8bd688c7
elguesabal <joseelguesabal@gmail.com>
2025-09-25 13:02:45 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
+import sharp from "sharp";
 
 const app = express();
 
@@ -14,12 +15,17 @@ app.get("/:diretorio/:arquivo", (req, res) => {
     res.status(404).send("Arquivo não encontrado");
 });
 
-app.get("/:diretorio/public/:arquivo", (req, res) => {
+app.get("/:diretorio/public/:arquivo", async (req, res) => {
     const { diretorio, arquivo } = req.params;
     const filePath = path.join(process.cwd(), "public", diretorio, "public", arquivo);
 
-    if (fs.existsSync(filePath)) return (res.sendFile(filePath));
-    res.status(404).send("Arquivo não encontrado");
+    if (!fs.existsSync(filePath)) return res.sendStatus(404);
+    try {
+        const buffer = await sharp(filePath).resize(800).jpeg({ quality: 70 }).toBuffer();
+        res.type("image/jpeg").send(buffer);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
